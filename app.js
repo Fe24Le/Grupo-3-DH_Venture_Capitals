64c97a7cc015251126f2a84e8e402f080b83b709
Fernando Calisaya <legasy23dream@gmail.com>
2024-01-26 02:28:18 +0100

    v7

--- app.js
+++ app.js
@@ -10,24 +10,26 @@ const port = process.env.PORT || 3001
 
 app.listen(port, () => console.log(`Corriendo puerto ${port}`));
 
+/*app.use('/',(req,res) => {res.status(200).send('Api Funciona Correctamente')});*/
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/index.html"))
 })
 
-app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/registro.html'))
+app.get("/register.html", (req, res) => {
+    res.sendFile(path.join(__dirname, './views/registro.html'))
 })
 
-app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/login.html'))
+app.get("/login.html", (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
 })
 
-app.get("/productDetail", (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/productDetail.html'))
+app.get("/productDetail.html", (req, res) => {
+    res.sendFile(path.join(__dirname, './views/productDetail.html'))
 })
 
-app.get("productCar", (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/productCar.html'))
+app.get("/productCar.html", (req, res) => {
+    res.sendFile(path.join(__dirname, './views/productCar.html'))
 })
 
 
